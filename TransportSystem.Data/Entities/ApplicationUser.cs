2b302b69d007f79f87b5b8744cfd601d44b141b7
Richard Nwohan <nwonahr@gmail.com>
2021-09-27 22:51:33 +0100

    added data layer

---
+++ TransportSystem.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportSystem.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string NextOfKinName { get; set; }
+        public string NextOfKinPhoneNumber { get; set; }
+
+    }
+}
